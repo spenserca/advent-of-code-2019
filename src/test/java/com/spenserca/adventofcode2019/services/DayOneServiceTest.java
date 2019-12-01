c2d89847730348ef8c3cb47f8ba310a11acb0cd1
Spenser Andreassen <spenserca@gmail.com>
2019-12-01 10:15:30 -0600

    test: add test for list calculation

--- src/test/java/com/spenserca/adventofcode2019/services/DayOneServiceTest.java
+++ src/test/java/com/spenserca/adventofcode2019/services/DayOneServiceTest.java
@@ -2,13 +2,16 @@ package com.spenserca.adventofcode2019.services;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DayOneServiceTest {
     private DayOneService underTest = new DayOneService();
 
     @Test
-    public void partOne_CalledWithMassOf12_Returns2() {
+    public void calculateFuelRequirement_CalledWithMassOf12_Returns2() {
         int expected = 2;
 
         int actual = underTest.calculateFuelRequirement(12);
@@ -17,7 +20,7 @@ public class DayOneServiceTest {
     }
 
     @Test
-    public void partOne_CalledWithMassOf14_Returns2() {
+    public void calculateFuelRequirement_CalledWithMassOf14_Returns2() {
         int expected = 2;
 
         int actual = underTest.calculateFuelRequirement(14);
@@ -26,7 +29,7 @@ public class DayOneServiceTest {
     }
 
     @Test
-    public void partOne_CalledWithMassOf17_Returns3() {
+    public void calculateFuelRequirement_CalledWithMassOf17_Returns3() {
         int expected = 3;
 
         int actual = underTest.calculateFuelRequirement(17);
@@ -35,7 +38,7 @@ public class DayOneServiceTest {
     }
 
     @Test
-    public void partOne_CalledWithMassOf1969_Returns654() {
+    public void calculateFuelRequirement_CalledWithMassOf1969_Returns654() {
         int expected = 654;
 
         int actual = underTest.calculateFuelRequirement(1969);
@@ -44,10 +47,20 @@ public class DayOneServiceTest {
     }
 
     @Test
-    public void partOne_CalledWithMassOf100756_Returns33583() {
+    public void calculateFuelRequirement_CalledWithMassOf100756_Returns33583() {
         int expected = 33583;
 
         int actual = underTest.calculateFuelRequirement(100756);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void calculateFuelRequirement_CalledWithListOfStringMasses_ReturnsExpectedResult() {
+        List<String> masses = Collections.singletonList("100756");
+        int expected = 33583;
+
+        int actual = underTest.calculateFuelRequirement(masses);
 
         assertThat(actual).isEqualTo(expected);
     }
